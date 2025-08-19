0f53373e5dc0ce8a6767662d561c5f766e85013a
SamnanAbdulJaleel <nabfar2017@gmail.com>
2025-08-19 17:38:25 +0530

    [CODE] FIXED MINOR CHANGES

--- src/constants/projects.ts
+++ src/constants/projects.ts
@@ -2,61 +2,61 @@ export const PROJECTS = [
   {
     title: "Restaurnt Billing System",
     desc: "A comprehensive billing system for restaurants, allowing for efficient management of orders and payments.",
-    link: "https://github.com/SamnanAbdulJaleel/Restaurant-Billing-System",
+    link: "https://github.com/TheSaMNaN/Restaurant-Billing-System",
     img_src: "/images/Screenshot2.png",
   },
   {
-    title: "Portfolio",
-    desc: "lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, voluptatibus.",
-    link: "https://github.com/SamnanAbdulJaleel/Restaurant-Billing-System",
+    title: "Portfolio Website",
+    desc: "This is my personal developer portfolio built with Next.js, Tailwind CSS, and TypeScript. It showcases my projects, skills, and experiences in a modern and responsive design",
+    link: "https://github.com/TheSaMNaN/Portfolio",
 
     img_src: "/images/projects/portfolio.png",
   },
   {
-    title: "Portfolio",
-    desc: "lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, voluptatibus.",
+    title: "Placeholder",
+    desc: "Just a placeholder to be replaced later for project description.",
     link: "https://acme.com",
 
     img_src: "/images/projects/portfolio.png",
   },
   {
-    title: "Portfolio",
-    desc: "lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, voluptatibus.",
+    title: "Placeholder",
+    desc: "Just a placeholder to be replaced later for project description.",
     link: "https://acme.com",
 
     img_src: "/images/projects/portfolio.png",
   },
   {
-    title: "Portfolio",
-    desc: "lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, voluptatibus.",
+    title: "Placeholder",
+    desc: "Just a placeholder to be replaced later for project description.",
     link: "https://acme.com",
 
     img_src: "/images/projects/portfolio.png",
   },
   {
-    title: "Portfolio",
-    desc: "lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, voluptatibus.",
+    title: "Placeholder",
+    desc: "Just a placeholder to be replaced later for project description.",
     link: "https://acme.com",
 
     img_src: "/images/projects/portfolio.png",
   },
   {
-    title: "Portfolio",
-    desc: "lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, voluptatibus.",
+    title: "Placeholder",
+    desc: "Just a placeholder to be replaced later for project description.",
     link: "https://acme.com",
 
     img_src: "/images/projects/portfolio.png",
   },
   {
-    title: "Portfolio",
-    desc: "lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, voluptatibus.",
+    title: "Placeholder",
+    desc: "Just a placeholder to be replaced later for project description.",
     link: "https://acme.com",
 
     img_src: "/images/projects/portfolio.png",
   },
   {
-    title: "Portfolio",
-    desc: "lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, voluptatibus.",
+    title: "Placeholder",
+    desc: "Just a placeholder to be replaced later for project description.",
     link: "https://acme.com",
 
     img_src: "/images/projects/portfolio.png",
